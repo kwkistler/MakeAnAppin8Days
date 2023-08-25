1fc2719de69f1b32495a8d9ab1a064a802fc2890
Kraig Kistler <kraigwkistler@gmail.com>
2023-08-25 15:39:16 -0700

    Initial Commit

---
+++ MakeAnAppin8Days/MakeAnAppin8DaysApp.swift
@@ -0,0 +1,17 @@
+//
+//  MakeAnAppin8DaysApp.swift
+//  MakeAnAppin8Days
+//
+//  Created by Kraig Kistler on 8/25/23.
+//
+
+import SwiftUI
+
+@main
+struct MakeAnAppin8DaysApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
